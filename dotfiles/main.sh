89591a009938abf2f3944784ce2d56f945cb6ef1
GiuffreV <giuffrevendetta@gmail.com>
2024-05-03 03:24:35 +0000

    adding pacman -Syu to main.sh

--- dotfiles/main.sh
+++ dotfiles/main.sh
@@ -2,7 +2,7 @@
 
 user=$(whoami)
 
-sudo pacman -S ttf-font-awesome sddm neofetch lf hyprland kitty rofi qt5-wayland dunst xdg-desktop-portal-hyprland qt6-wayland hyprpaper waybar pipewire pipewire-audio pipewire-alsa pipewire-pulse spotify-launcher firefox vscode nano
+sudo pacman -Syu ttf-font-awesome sddm neofetch lf hyprland kitty rofi qt5-wayland dunst xdg-desktop-portal-hyprland qt6-wayland hyprpaper waybar pipewire pipewire-audio pipewire-alsa pipewire-pulse spotify-launcher firefox vscode nano
 curl -sS https://starship.rs/install.sh | sh
 
 sleep 1
